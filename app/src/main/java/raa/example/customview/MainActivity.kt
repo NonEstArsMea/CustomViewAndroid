f1147ce88bafe9285a2bb370b4ff24ac4eb3d680
Ruabov <kudakov08@gmail.com>
2024-03-04 13:13:29 +0300

    add columns

--- app/src/main/java/raa/example/customview/MainActivity.kt
+++ app/src/main/java/raa/example/customview/MainActivity.kt
@@ -3,6 +3,7 @@ package raa.example.customview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import raa.example.customview.gantsView.GantView
+import raa.example.customview.gantsView.NewView
 import raa.example.customview.gantsView.Task
 import java.time.LocalDate
 
@@ -10,6 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val view = findViewById<NewView>(R.id.gant)
+
+        view.setTimeTable(listOf("123","456"))
 
     }
 }
--- app/src/main/java/raa/example/customview/gantsView/NewView.kt
+++ app/src/main/java/raa/example/customview/gantsView/NewView.kt
@@ -3,10 +3,17 @@ package raa.example.customview.gantsView
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Matrix
 import android.graphics.Paint
+import android.graphics.Path
 import android.graphics.Rect
+import android.graphics.RectF
+import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
+import raa.example.customview.R
+import java.time.LocalDate
 
 class NewView @JvmOverloads constructor(
     context: Context,
@@ -18,6 +25,10 @@ class NewView @JvmOverloads constructor(
     private val minRowHight = 150
     private val namesRowHight = 120
 
+    // Отвечает за зум и сдвиги
+    private val transformations = Transformations()
+
+
     private val rowPaint = Paint().apply {
         style = Paint.Style.FILL
         color = Color.RED
@@ -28,6 +39,14 @@ class NewView @JvmOverloads constructor(
         color = Color.GRAY
     }
 
+    private val contentWidth: Int
+        get() = (periodWidth * 6).toInt()
+
+
+    private val periodNamePaint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = resources.getDimension(R.dimen.gant_period_name_text_size)
+        color = ContextCompat.getColor(context, R.color.grey_500)
+    }
 
     // Rect для рисования строк
     private val rowRect = Rect()
@@ -71,18 +90,121 @@ class NewView @JvmOverloads constructor(
 
 
     private fun Canvas.drawRows() {
-            repeat(COUNT_OF_DAYS + 1) { index ->
-                rowRect.offsetTo(0, index * minRowHight + namesRowHight)
-                rowPaint.color = rowColors[index % 2]
-                drawRect(rowRect, rowPaint)
-            }
+        repeat(COUNT_OF_DAYS + 1) { index ->
+            rowRect.offsetTo(0, index * minRowHight + namesRowHight)
+            rowPaint.color = rowColors[index % 2]
+            drawRect(rowRect, rowPaint)
+        }
     }
+
+
+
+    private fun Canvas.drawPeriods() {
+        val currentPeriods = listOf("123","345","345","34522222","342222225")
+        val nameY = periodNamePaint.getTextBaselineByCenter(minRowHight / 2f)
+        currentPeriods.forEachIndexed { index, periodName ->
+            // По X текст рисуется относительно его начала
+            val textWidth = periodNamePaint.measureText(periodName)
+            val periodCenter = periodWidth * transformations.scaleX * (index + 0.5f)
+            val nameX = (periodCenter - (textWidth / 2)) + transformations.translationX
+            drawText(periodName, nameX, nameY, periodNamePaint)
+            // Разделитель
+            val separatorX = periodWidth * (index + 1f) * transformations.scaleX + transformations.translationX
+            drawLine(separatorX, 0f, separatorX, height.toFloat(), separatorsPaint)
+        }
+    }
+
+    private fun Paint.getTextBaselineByCenter(center: Float) = center - (descent() + ascent()) / 2
 
     override fun onDraw(canvas: Canvas) = with(canvas) {
         drawRows()
+        drawPeriods()
     }
+
+
+    private var timeTable: List<String> = emptyList()
+    private var uiTimeTable: List<String> = emptyList()
+
+    private val periodWidth = 100f
+
+    fun setTimeTable(timeTable: List<String>) {
+        if (timeTable != this.timeTable) {
+            this.timeTable = timeTable
+            //uiTimeTable = timeTable.map(::)
+            // Сообщаем, что нужно пересчитать размеры
+            requestLayout()
+            // Сообщаем, что нужно перерисоваться
+            invalidate()
+        }
+    }
+
+    private inner class Transformations {
+        var translationX = 0f
+            private set
+        var scaleX = 1f
+            private set
+
+        // Матрица для преобразования фигур тасок
+        private val matrix = Matrix()
+
+        // На сколько максимально можно сдвинуть диаграмму
+        private val minTranslation: Float
+            get() = (width - contentWidth * transformations.scaleX).coerceAtMost(0f)
+
+        // Относительный сдвиг на dx
+        fun addTranslation(dx: Float) {
+            translationX = (translationX + dx).coerceIn(minTranslation, 0f)
+        }
+
+        // Относительное увеличение на sx
+        fun addScale(sx: Float) {
+            scaleX = (scaleX * sx).coerceIn(1f, MAX_SCALE)
+            recalculateTranslationX()
+        }
+
+
+
+        // Когда изменился scale или размер View надо пересчитать сдвиг
+        private fun recalculateTranslationX() {
+            translationX = translationX.coerceIn(minTranslation, 0f)
+        }
+
+    }
+
+//    private inner class UiTask(val task: Task) {
+//        // Rect с учетом всех преобразований
+//        val rect = RectF()
+//
+//        // Начальный Rect для текущих размеров View
+//        private val untransformedRect = RectF()
+//
+//        // Если false, таск рисовать не нужно
+//        val isRectOnScreen: Boolean
+//            get() = rect.top < height && (rect.right > 0 || rect.left < width)
+//
+//        fun updateInitialRect(index: Int) {
+//            fun getX(date: LocalDate): Float? {
+//                val periodIndex =
+//                    periods.getValue(periodType).indexOf(periodType.getDateString(date))
+//                return if (periodIndex >= 0) {
+//                    periodWidth * (periodIndex + periodType.getPercentOfPeriod(date))
+//                } else {
+//                    null
+//                }
+//            }
+//
+//            untransformedRect.set(
+//                getX(task.dateStart) ?: -5f,
+//                40f * (index + 1f),
+//                getX(task.dateEnd) ?: width.toFloat(),
+//                40f * (index + 2f),
+//            )
+//            rect.set(untransformedRect)
+//        }
+//    }
 
     companion object {
         const val COUNT_OF_DAYS = 16
+        private const val MAX_SCALE = 2f
     }
 }
